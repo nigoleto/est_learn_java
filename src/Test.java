15ffda9ec632e364cc88180bb463952c26cf7b83
nigoleto <nigoleto@gmail.com>
2024-05-30 09:34:48 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,34 @@
+public class Test {
+
+    /**
+     * @author 굿
+     * @deprecated 사용중단을 의미한다.
+     * @apiNote apinote 이것을 JavaDoc 이라고 한다.
+     *      * 클래스에 대한 설명을 상세하게 다는 역할을 한다.
+     * @since 2024-05-29
+     **/
+    public static void main(String[] args) {
+        int CtrlAltShiftJ = 10;
+        System.out.println(CtrlAltShiftJ);
+        System.out.println(CtrlAltShiftJ);
+        System.out.println(CtrlAltShiftJ);
+        System.out.println(CtrlAltShiftJ);
+        System.out.println(CtrlAltShiftJ);
+        System.out.println(CtrlAltShiftJ);
+        System.out.println(CtrlAltShiftJ);
+        System.out.println(CtrlAltShiftJ);
+
+
+//        ctrl+/
+
+
+/*
+
+
+        ctrl + shift + /
+
+         */
+
+
+    }
+}
