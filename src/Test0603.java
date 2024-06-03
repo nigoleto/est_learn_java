f0fb2181033e230f4520661cb743732958068175
nigoleto <nigoleto@gmail.com>
2024-06-03 11:43:30 +0900

    240603

---
+++ src/Test0603.java
@@ -0,0 +1,125 @@
+public class Test0603 {
+  public static void main(String[] args) {
+    //        int number1 = 10;
+    //        int number2 = 3;
+    //
+    //        int sum = number1 + number2;
+    //        int diff1 = number2 - number1;
+    //        int diff2 = number1 - number2;
+    //
+    //        int product = number1 * number2;
+    //        int quot = number1 / number2;
+    //        int remainder = number1 % number2;
+    //
+    //        System.out.println("덧셈"+sum);
+    //        System.out.println("뺄셈1"+diff1);
+    //        System.out.println("뺄셈2"+diff1);
+    //        System.out.println("곱셈"+product);
+    //        System.out.println("나눗셈"+quot);
+    //        System.out.println("나머지"+remainder);
+
+    //        -------------------------------------
+    //        byte a = 1;
+    //        byte b = 2;
+    ////        byte c = a + b; // 이게 안된다.
+    //
+    //        -------------------------------------
+    //            int a = 10;
+    //            int b = 4;
+    //            int c = a / b;
+    //            double d = a / b; // 이미 a/b를 계산할 때에 정수로 계산 해서 2가되고, 그 뒤에 2.0으로 만든다.
+    //            double e = (double) a / b; // 형변환을 먼저하고 나눠진다.
+    //
+    //            System.out.println(d);
+    //            System.out.println(e);
+
+    //        -------------------------------------
+    //    char c1 = 'A' + 1;
+    //    char c2 = 'A';
+    //    char c3 = c2 + 1; //char이 정수형 데이터라서 int로 표현됨
+
+    //        -------------------------------------
+    //      String str1 = "안녕하세요. ";
+    //      String str2 = "만나서 반갑습니다.";
+    //      String str3 = str1 + str2;
+    //
+    //    System.out.println(str3);
+
+    //        -------------------------------------
+    //    System.out.println("Hello" + 123 + 456); // Hello123456
+    //    System.out.println(123 + 456 + "Hello"); // 579Hello
+
+    //        -------------------------------------
+    //    StringBuilder sb = new StringBuilder();
+    //    sb.append(123);
+    //    sb.append(456);
+    //    sb.append("Hello");
+    //    System.out.println(sb); // 123456Hello
+    //
+    //        -------------------------------------
+    //    int number1 = 10;
+    //    int number2 = 30;
+    //
+    //    System.out.println(number1 > number2);
+    //    System.out.println(number1 >= number2);
+    //    System.out.println(number1 < number2);
+    //    System.out.println(number1 <= number2);
+    //    System.out.println(number1 == number2);
+    //    System.out.println(number1 != number2);
+
+    //        -------------------------------------
+
+    //    String a = new String();
+    //    String b = new String();
+    //    System.out.println(a==b); // false
+    //    System.out.println(a.equals(b)); // true
+
+    //        -------------------------------------
+
+    //    System.out.println(true && true);
+    //    System.out.println(true && false);
+    //    System.out.println(false && true);
+    //    System.out.println(false && false);
+
+    //    System.out.println(true || true);
+    //    System.out.println(true || false);
+    //    System.out.println(false || true);
+    //    System.out.println(false || false);
+
+    //        -------------------------------------
+
+    //    i % 2 == 0 && i % 3 == 0
+
+    //    int x = 5;
+    //    System.out.println(x % 30 == 0 && (x % 2 == 0 || x % 5 == 0));
+
+    // x는 30의 배수이면서 2의 배수이거나 5의 배수이다.
+    //  x는 (30의 배수이면서 2의 배수)이거나 (5의 배수)이다.
+
+    //        -------------------------------------
+    //    System.out.println(true|true);
+    //    System.out.println(true|false);
+    //    System.out.println(false|false);
+    //
+    //    System.out.println(0b0100);
+    //    System.out.println(00100);
+
+    //        -------------------------------------
+    //    int score = 95;
+    //    String grade = (score == 90 && score % 5 == 0) ?  "GOOD" : "BAD";
+
+//    int score = 90;
+//    String grade;
+//
+//    if (score > 90) {
+//      grade = "A";
+//    } else {
+//      grade = "B";
+//    }
+//    System.out.println(grade);
+
+    //        -------------------------------------
+
+
+  }
+}
